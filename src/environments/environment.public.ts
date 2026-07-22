439203ed0edf79a7e2a68f6e4004b8d45b787263
asistente02 <asistente02metaverso@gmail.com>
2026-07-21 23:08:55 -0500

    Estilo

--- src/environments/environment.public.ts
+++ src/environments/environment.public.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://geriafab-backend-505v.onrender.com',
+  apiBaseUrl: 'https://geriafab-backend.onrender.com',
   youtubeApiKey: 'AIzaSyBDVGtx8BR9gPMXymgMSaCJTYfUt9AuujA',
 };
